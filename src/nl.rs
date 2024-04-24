36941459e90d9a23f6aba1aad6dea7b27520b0bc
John T. Wodder II <git@varonathe.org>
2024-04-24 14:36:05 -0400

    as_char() & lengths

--- src/nl.rs
+++ src/nl.rs
@@ -35,10 +35,59 @@ impl Newline {
             Newline::ParagraphSeparator => "\u{2029}",
         }
     }
+
+    pub fn as_char(&self) -> Option<char> {
+        match self {
+            Newline::LineFeed => Some('\n'),
+            Newline::CarriageReturn => Some('\r'),
+            Newline::CrLf => None,
+            Newline::VerticalTab => Some('\x0B'),
+            Newline::FormFeed => Some('\x0C'),
+            Newline::NextLine => Some('\u{0085}'),
+            Newline::LineSeparator => Some('\u{2028}'),
+            Newline::ParagraphSeparator => Some('\u{2029}'),
+        }
+    }
+
+    pub fn len_char(&self) -> usize {
+        match self {
+            Newline::CrLf => 2,
+            _ => 1,
+        }
+    }
+
+    pub fn len_utf8(&self) -> usize {
+        self.as_str().len()
+    }
 }
 
 impl fmt::Display for Newline {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(self.as_str())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_len_char() {
+        for nl in Newline::iter() {
+            assert_eq!(nl.len_char(), nl.as_str().chars().count());
+        }
+    }
+
+    #[test]
+    fn test_as_char() {
+        for nl in Newline::iter() {
+            let mut chiter = nl.as_str().chars();
+            let c1 = chiter.next().unwrap();
+            if chiter.next().is_some() {
+                assert_eq!(nl.as_char(), None);
+            } else {
+                assert_eq!(nl.as_char(), Some(c1));
+            }
+        }
     }
 }
