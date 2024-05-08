dea6f4b6c437fe6974981c84aeb9f4ad451c8429
John T. Wodder II <git@varonathe.org>
2024-05-08 15:36:29 -0400

    Replace `Match` type with just `(start index, end index)`

--- src/traits.rs
+++ src/traits.rs
@@ -10,117 +10,74 @@ mod private {
 }
 
 pub trait NewlinePattern: private::Sealed {
-    fn search<'a>(&self, s: &'a str) -> Option<Match<'a>>;
-    fn rsearch<'a>(&self, s: &'a str) -> Option<Match<'a>>;
+    fn search(&self, s: &str) -> Option<(usize, usize)>;
+    fn rsearch(&self, s: &str) -> Option<(usize, usize)>;
 }
 
 impl NewlinePattern for Newline {
-    fn search<'a>(&self, s: &'a str) -> Option<Match<'a>> {
+    fn search(&self, s: &str) -> Option<(usize, usize)> {
         let start = s.find(self.as_str())?;
         let end = start.saturating_add(self.len_utf8());
-        Some(Match {
-            start,
-            end,
-            newline: *self,
-            before: &s[..start],
-            after: &s[end..],
-        })
+        Some((start, end))
     }
 
-    fn rsearch<'a>(&self, s: &'a str) -> Option<Match<'a>> {
+    fn rsearch(&self, s: &str) -> Option<(usize, usize)> {
         let start = s.rfind(self.as_str())?;
         let end = start.saturating_add(self.len_utf8());
-        Some(Match {
-            start,
-            end,
-            newline: *self,
-            before: &s[..start],
-            after: &s[end..],
-        })
+        Some((start, end))
     }
 }
 
 impl NewlinePattern for NewlineSet {
-    fn search<'a>(&self, s: &'a str) -> Option<Match<'a>> {
+    fn search(&self, s: &str) -> Option<(usize, usize)> {
         if self.is_empty() {
             return None;
         }
         let start = s.find(self.pattern())?;
-        let newline = if self.crlf && s[start..].starts_with("\r\n") {
-            Newline::CrLf
+        let length = if self.crlf && s[start..].starts_with("\r\n") {
+            2
         } else {
             let Some(ch) = s[start..].chars().next() else {
                 unreachable!(
                     "Nonempty NewlineSet pattern should have matched at start of a character"
                 )
             };
-            match Newline::try_from(ch) {
-                Ok(nl) => nl,
-                Err(_) => unreachable!(
-                    "NewlineSet pattern should have matched a char that maps to Newline"
-                ),
-            }
+            ch.len_utf8()
         };
-        let end = start.saturating_add(newline.len_utf8());
-        Some(Match {
-            start,
-            end,
-            newline,
-            before: &s[..start],
-            after: &s[end..],
-        })
+        let end = start.saturating_add(length);
+        Some((start, end))
     }
 
-    fn rsearch<'a>(&self, s: &'a str) -> Option<Match<'a>> {
+    fn rsearch(&self, s: &str) -> Option<(usize, usize)> {
         if self.is_empty() {
             return None;
         }
         let mut s_end = s.len();
         loop {
             let mut start = s[..s_end].rfind(self.pattern())?;
-            let newline = match (self.crlf, self.pattern.contains('\n')) {
+            let length = match (self.crlf, self.pattern.contains('\n')) {
                 (true, true) if s[start..].starts_with('\n') && s[..start].ends_with('\r') => {
                     start -= 1;
-                    Newline::CrLf
+                    2
                 }
-                (true, false) if s[start..].starts_with("\r\n") => Newline::CrLf,
+                (true, false) if s[start..].starts_with("\r\n") => 2,
                 _ => {
                     let Some(ch) = s[start..].chars().next() else {
                         unreachable!(
                         "Nonempty NewlineSet pattern should have matched at start of a character"
                     )
                     };
-                    match Newline::try_from(ch) {
-                        Ok(Newline::CarriageReturn) if !self.cr => {
-                            s_end = start;
-                            continue;
-                        }
-                        Ok(nl) => nl,
-                        Err(_) => unreachable!(
-                            "NewlineSet pattern should have matched a char that maps to Newline"
-                        ),
+                    if !self.cr && ch == '\r' {
+                        s_end = start;
+                        continue;
                     }
+                    ch.len_utf8()
                 }
             };
-            let end = start.saturating_add(newline.len_utf8());
-            return Some(Match {
-                start,
-                end,
-                newline,
-                before: &s[..start],
-                after: &s[end..],
-            });
+            let end = start.saturating_add(length);
+            return Some((start, end));
         }
     }
-}
-
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Match<'a> {
-    pub start: usize,
-    pub end: usize,
-    pub newline: Newline,
-    pub before: &'a str,
-    pub after: &'a str,
 }
 
 #[cfg(test)]
@@ -130,76 +87,25 @@ mod tests {
 
     #[rstest]
     #[case(Newline::LineFeed, "foobar", None)]
-    #[case(Newline::LineFeed, "foo\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::LineFeed,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::LineFeed, "\nfoobar", Some(Match {
-        start: 0,
-        end: 1,
-        newline: Newline::LineFeed,
-        before: "",
-        after: "foobar",
-    }))]
-    #[case(Newline::LineFeed, "foobar\n", Some(Match {
-        start: 6,
-        end: 7,
-        newline: Newline::LineFeed,
-        before: "foobar",
-        after: "",
-    }))]
+    #[case(Newline::LineFeed, "foo\nbar", Some((3, 4)))]
+    #[case(Newline::LineFeed, "\nfoobar", Some((0, 1)))]
+    #[case(Newline::LineFeed, "foobar\n", Some((6, 7)))]
     #[case(Newline::LineFeed, "foo\rbar", None)]
     #[case(Newline::CrLf, "foo\rbar", None)]
     #[case(Newline::CrLf, "foo\nbar", None)]
-    #[case(Newline::CarriageReturn, "foo\rbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "\nbar",
-    }))]
-    #[case(Newline::CrLf, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CrLf, "foo\rbar\r\nquux", Some(Match {
-        start: 7,
-        end: 9,
-        newline: Newline::CrLf,
-        before: "foo\rbar",
-        after: "quux",
-    }))]
-    #[case(Newline::LineSeparator, "foo\u{2028}bar", Some(Match {
-        start: 3,
-        end: 6,
-        newline: Newline::LineSeparator,
-        before: "foo",
-        after: "bar",
-    }))]
+    #[case(Newline::CarriageReturn, "foo\rbar", Some((3, 4)))]
+    #[case(Newline::CarriageReturn, "foo\r\nbar", Some((3, 4)))]
+    #[case(Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+    #[case(Newline::CrLf, "foo\rbar\r\nquux", Some((7, 9)))]
+    #[case(Newline::LineSeparator, "foo\u{2028}bar", Some((3, 6)))]
     fn test_newline_search(
         #[case] nl: Newline,
-        #[case] s: &'static str,
-        #[case] m: Option<Match<'static>>,
+        #[case] s: &str,
+        #[case] m: Option<(usize, usize)>,
     ) {
         assert_eq!(nl.search(s), m);
-        if let Some(m) = m {
-            assert_eq!(nl, m.newline);
-            assert_eq!(&s[..m.start], m.before);
-            assert_eq!(&s[m.end..], m.after);
-            assert_eq!(&s[m.start..m.end], m.newline.as_str());
+        if let Some((start, end)) = m {
+            assert_eq!(&s[start..end], nl.as_str());
         }
     }
 
@@ -207,267 +113,78 @@ mod tests {
     #[case(NewlineSet::EMPTY, "foobar", None)]
     #[case(NewlineSet::EMPTY, "foo\r\nbar", None)]
     #[case(Newline::LineFeed.into(), "foobar", None)]
-    #[case(Newline::LineFeed.into(), "foo\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::LineFeed,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::LineFeed.into(), "foo\r\nbar", Some(Match {
-        start: 4,
-        end: 5,
-        newline: Newline::LineFeed,
-        before: "foo\r",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar\r\nquux", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar\r\nquux",
-    }))]
-    #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "\nbar",
-    }))]
-    #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some(Match {
-        start: 3,
-        end: 6,
-        newline: Newline::LineSeparator,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::LineFeed | Newline::CarriageReturn, "foo\rbar\nquux", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar\nquux",
-    }))]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
+    #[case(Newline::LineFeed.into(), "foo\nbar", Some((3, 4)))]
+    #[case(Newline::LineFeed.into(), "foo\r\nbar", Some((4, 5)))]
+    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some((3, 4)))]
+    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar\r\nquux", Some((3, 4)))]
+    #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some((3, 4)))]
+    #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some((3, 6)))]
+    #[case(Newline::LineFeed | Newline::CarriageReturn, "foo\rbar\nquux", Some((3, 4)))]
+    #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
     fn test_newline_set_search(
         #[case] nlset: NewlineSet,
-        #[case] s: &'static str,
-        #[case] m: Option<Match<'static>>,
+        #[case] s: &str,
+        #[case] m: Option<(usize, usize)>,
     ) {
         assert_eq!(nlset.search(s), m);
-        if let Some(m) = m {
-            assert!(nlset.contains(m.newline));
-            assert_eq!(&s[..m.start], m.before);
-            assert_eq!(&s[m.end..], m.after);
-            assert_eq!(&s[m.start..m.end], m.newline.as_str());
+        if let Some((start, end)) = m {
+            assert!(nlset.contains(Newline::try_from(&s[start..end]).unwrap()));
         }
     }
 
     #[rstest]
     #[case(Newline::LineFeed, "foobar", None)]
-    #[case(Newline::LineFeed, "foo\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::LineFeed,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::LineFeed, "\nfoobar", Some(Match {
-        start: 0,
-        end: 1,
-        newline: Newline::LineFeed,
-        before: "",
-        after: "foobar",
-    }))]
-    #[case(Newline::LineFeed, "foobar\n", Some(Match {
-        start: 6,
-        end: 7,
-        newline: Newline::LineFeed,
-        before: "foobar",
-        after: "",
-    }))]
+    #[case(Newline::LineFeed, "foo\nbar", Some((3, 4)))]
+    #[case(Newline::LineFeed, "\nfoobar", Some((0, 1)))]
+    #[case(Newline::LineFeed, "foobar\n", Some((6, 7)))]
     #[case(Newline::LineFeed, "foo\rbar", None)]
     #[case(Newline::CrLf, "foo\rbar", None)]
     #[case(Newline::CrLf, "foo\nbar", None)]
-    #[case(Newline::CarriageReturn, "foo\rbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "\nbar",
-    }))]
-    #[case(Newline::CrLf, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CrLf, "foo\rbar\r\nquux", Some(Match {
-        start: 7,
-        end: 9,
-        newline: Newline::CrLf,
-        before: "foo\rbar",
-        after: "quux",
-    }))]
-    #[case(Newline::LineSeparator, "foo\u{2028}bar", Some(Match {
-        start: 3,
-        end: 6,
-        newline: Newline::LineSeparator,
-        before: "foo",
-        after: "bar",
-    }))]
+    #[case(Newline::CarriageReturn, "foo\rbar", Some((3, 4)))]
+    #[case(Newline::CarriageReturn, "foo\r\nbar", Some((3, 4)))]
+    #[case(Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+    #[case(Newline::CrLf, "foo\rbar\r\nquux", Some((7, 9)))]
+    #[case(Newline::LineSeparator, "foo\u{2028}bar", Some((3, 6)))]
     fn test_newline_rsearch(
         #[case] nl: Newline,
-        #[case] s: &'static str,
-        #[case] m: Option<Match<'static>>,
+        #[case] s: &str,
+        #[case] m: Option<(usize, usize)>,
     ) {
         assert_eq!(nl.rsearch(s), m);
-        if let Some(m) = m {
-            assert_eq!(nl, m.newline);
-            assert_eq!(&s[..m.start], m.before);
-            assert_eq!(&s[m.end..], m.after);
-            assert_eq!(&s[m.start..m.end], m.newline.as_str());
+        if let Some((start, end)) = m {
+            assert_eq!(&s[start..end], nl.as_str());
         }
     }
 
     #[rstest]
-    #[case(NewlineSet::ASCII, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "\nbar",
-    }))]
-    #[case(Newline::CrLf.into(), "foo\r\nbar", Some(Match {
-        start: 3,
-        end: 5,
-        newline: Newline::CrLf,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(NewlineSet::ASCII, "foo\n\rbar", Some(Match {
-        start: 4,
-        end: 5,
-        newline: Newline::CarriageReturn,
-        before: "foo\n",
-        after: "bar",
-    }))]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\n\rbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::LineFeed,
-        before: "foo",
-        after: "\rbar",
-    }))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\n\rbar", Some(Match {
-        start: 4,
-        end: 5,
-        newline: Newline::CarriageReturn,
-        before: "foo\n",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn.into(), "foo\n\rbar", Some(Match {
-        start: 4,
-        end: 5,
-        newline: Newline::CarriageReturn,
-        before: "foo\n",
-        after: "bar",
-    }))]
+    #[case(NewlineSet::ASCII, "foo\r\nbar", Some((3, 5)))]
+    #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+    #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some((3, 4)))]
+    #[case(Newline::CrLf.into(), "foo\r\nbar", Some((3, 5)))]
+    #[case(NewlineSet::ASCII, "foo\n\rbar", Some((4, 5)))]
+    #[case(Newline::LineFeed | Newline::CrLf, "foo\n\rbar", Some((3, 4)))]
+    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\n\rbar", Some((4, 5)))]
+    #[case(Newline::CarriageReturn.into(), "foo\n\rbar", Some((4, 5)))]
     #[case(Newline::CrLf.into(), "foo\n\rbar", None)]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\nbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::LineFeed,
-        before: "foo",
-        after: "bar",
-    }))]
+    #[case(Newline::LineFeed | Newline::CrLf, "foo\nbar", Some((3, 4)))]
     #[case(Newline::CarriageReturn | Newline::CrLf, "foo\nbar", None)]
     #[case(Newline::CarriageReturn.into(), "foo\nbar", None)]
     #[case(Newline::CrLf.into(), "foo\nbar", None)]
     #[case(Newline::LineFeed | Newline::CrLf, "foo\rbar", None)]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar",
-    }))]
-    #[case(Newline::CarriageReturn.into(), "foo\rbar", Some(Match {
-        start: 3,
-        end: 4,
-        newline: Newline::CarriageReturn,
-        before: "foo",
-        after: "bar",
-    }))]
+    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some((3, 4)))]
+    #[case(Newline::CarriageReturn.into(), "foo\rbar", Some((3, 4)))]
     #[case(Newline::CrLf.into(), "foo\rbar", None)]
-    #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some(Match {
-        start: 3,
-        end: 6,
-        newline: Newline::LineSeparator,
-        before: "foo",
-        after: "bar",
-    }))]
+    #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some((3, 6)))]
     fn test_newline_set_rsearch(
         #[case] nlset: NewlineSet,
-        #[case] s: &'static str,
-        #[case] m: Option<Match<'static>>,
+        #[case] s: &str,
+        #[case] m: Option<(usize, usize)>,
     ) {
         assert_eq!(nlset.rsearch(s), m);
-        if let Some(m) = m {
-            assert!(nlset.contains(m.newline));
-            assert_eq!(&s[..m.start], m.before);
-            assert_eq!(&s[m.end..], m.after);
-            assert_eq!(&s[m.start..m.end], m.newline.as_str());
+        if let Some((start, end)) = m {
+            assert!(nlset.contains(Newline::try_from(&s[start..end]).unwrap()));
         }
     }
 }
