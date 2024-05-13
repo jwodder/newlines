c8f9779712d76f54e87838202f2b9f7952b22822
John T. Wodder II <git@varonathe.org>
2024-05-12 22:59:38 -0400

    Organize pattern::tests into submodules

--- src/pattern.rs
+++ src/pattern.rs
@@ -85,106 +85,98 @@ mod tests {
     use super::*;
     use rstest::rstest;
 
-    #[rstest]
-    #[case(Newline::LineFeed, "foobar", None)]
-    #[case(Newline::LineFeed, "foo\nbar", Some((3, 4)))]
-    #[case(Newline::LineFeed, "\nfoobar", Some((0, 1)))]
-    #[case(Newline::LineFeed, "foobar\n", Some((6, 7)))]
-    #[case(Newline::LineFeed, "foo\rbar", None)]
-    #[case(Newline::CrLf, "foo\rbar", None)]
-    #[case(Newline::CrLf, "foo\nbar", None)]
-    #[case(Newline::CarriageReturn, "foo\rbar", Some((3, 4)))]
-    #[case(Newline::CarriageReturn, "foo\r\nbar", Some((3, 4)))]
-    #[case(Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
-    #[case(Newline::CrLf, "foo\rbar\r\nquux", Some((7, 9)))]
-    #[case(Newline::LineSeparator, "foo\u{2028}bar", Some((3, 6)))]
-    fn test_newline_search(
-        #[case] nl: Newline,
-        #[case] s: &str,
-        #[case] m: Option<(usize, usize)>,
-    ) {
-        assert_eq!(nl.search(s), m);
-        if let Some((start, end)) = m {
-            assert_eq!(&s[start..end], nl.as_str());
+    mod newline {
+        use super::*;
+
+        #[rstest]
+        #[case(Newline::LineFeed, "foobar", None)]
+        #[case(Newline::LineFeed, "foo\nbar", Some((3, 4)))]
+        #[case(Newline::LineFeed, "\nfoobar", Some((0, 1)))]
+        #[case(Newline::LineFeed, "foobar\n", Some((6, 7)))]
+        #[case(Newline::LineFeed, "foo\rbar", None)]
+        #[case(Newline::CrLf, "foo\rbar", None)]
+        #[case(Newline::CrLf, "foo\nbar", None)]
+        #[case(Newline::CarriageReturn, "foo\rbar", Some((3, 4)))]
+        #[case(Newline::CarriageReturn, "foo\r\nbar", Some((3, 4)))]
+        #[case(Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+        #[case(Newline::CrLf, "foo\rbar\r\nquux", Some((7, 9)))]
+        #[case(Newline::LineSeparator, "foo\u{2028}bar", Some((3, 6)))]
+        fn search(#[case] nl: Newline, #[case] s: &str, #[case] m: Option<(usize, usize)>) {
+            assert_eq!(nl.search(s), m);
+            if let Some((start, end)) = m {
+                assert_eq!(&s[start..end], nl.as_str());
+            }
+        }
+
+        #[rstest]
+        #[case(Newline::LineFeed, "foobar", None)]
+        #[case(Newline::LineFeed, "foo\nbar", Some((3, 4)))]
+        #[case(Newline::LineFeed, "\nfoobar", Some((0, 1)))]
+        #[case(Newline::LineFeed, "foobar\n", Some((6, 7)))]
+        #[case(Newline::LineFeed, "foo\rbar", None)]
+        #[case(Newline::CrLf, "foo\rbar", None)]
+        #[case(Newline::CrLf, "foo\nbar", None)]
+        #[case(Newline::CarriageReturn, "foo\rbar", Some((3, 4)))]
+        #[case(Newline::CarriageReturn, "foo\r\nbar", Some((3, 4)))]
+        #[case(Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+        #[case(Newline::CrLf, "foo\rbar\r\nquux", Some((7, 9)))]
+        #[case(Newline::LineSeparator, "foo\u{2028}bar", Some((3, 6)))]
+        fn rsearch(#[case] nl: Newline, #[case] s: &str, #[case] m: Option<(usize, usize)>) {
+            assert_eq!(nl.rsearch(s), m);
+            if let Some((start, end)) = m {
+                assert_eq!(&s[start..end], nl.as_str());
+            }
         }
     }
 
-    #[rstest]
-    #[case(NewlineSet::EMPTY, "foobar", None)]
-    #[case(NewlineSet::EMPTY, "foo\r\nbar", None)]
-    #[case(Newline::LineFeed.into(), "foobar", None)]
-    #[case(Newline::LineFeed.into(), "foo\nbar", Some((3, 4)))]
-    #[case(Newline::LineFeed.into(), "foo\r\nbar", Some((4, 5)))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some((3, 4)))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar\r\nquux", Some((3, 4)))]
-    #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some((3, 4)))]
-    #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some((3, 6)))]
-    #[case(Newline::LineFeed | Newline::CarriageReturn, "foo\rbar\nquux", Some((3, 4)))]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
-    fn test_newline_set_search(
-        #[case] nlset: NewlineSet,
-        #[case] s: &str,
-        #[case] m: Option<(usize, usize)>,
-    ) {
-        assert_eq!(nlset.search(s), m);
-        if let Some((start, end)) = m {
-            assert!(nlset.contains(Newline::try_from(&s[start..end]).unwrap()));
-        }
-    }
+    mod newline_set {
+        use super::*;
 
-    #[rstest]
-    #[case(Newline::LineFeed, "foobar", None)]
-    #[case(Newline::LineFeed, "foo\nbar", Some((3, 4)))]
-    #[case(Newline::LineFeed, "\nfoobar", Some((0, 1)))]
-    #[case(Newline::LineFeed, "foobar\n", Some((6, 7)))]
-    #[case(Newline::LineFeed, "foo\rbar", None)]
-    #[case(Newline::CrLf, "foo\rbar", None)]
-    #[case(Newline::CrLf, "foo\nbar", None)]
-    #[case(Newline::CarriageReturn, "foo\rbar", Some((3, 4)))]
-    #[case(Newline::CarriageReturn, "foo\r\nbar", Some((3, 4)))]
-    #[case(Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
-    #[case(Newline::CrLf, "foo\rbar\r\nquux", Some((7, 9)))]
-    #[case(Newline::LineSeparator, "foo\u{2028}bar", Some((3, 6)))]
-    fn test_newline_rsearch(
-        #[case] nl: Newline,
-        #[case] s: &str,
-        #[case] m: Option<(usize, usize)>,
-    ) {
-        assert_eq!(nl.rsearch(s), m);
-        if let Some((start, end)) = m {
-            assert_eq!(&s[start..end], nl.as_str());
+        #[rstest]
+        #[case(NewlineSet::EMPTY, "foobar", None)]
+        #[case(NewlineSet::EMPTY, "foo\r\nbar", None)]
+        #[case(Newline::LineFeed.into(), "foobar", None)]
+        #[case(Newline::LineFeed.into(), "foo\nbar", Some((3, 4)))]
+        #[case(Newline::LineFeed.into(), "foo\r\nbar", Some((4, 5)))]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some((3, 4)))]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar\r\nquux", Some((3, 4)))]
+        #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some((3, 4)))]
+        #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some((3, 6)))]
+        #[case(Newline::LineFeed | Newline::CarriageReturn, "foo\rbar\nquux", Some((3, 4)))]
+        #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+        fn search(#[case] nlset: NewlineSet, #[case] s: &str, #[case] m: Option<(usize, usize)>) {
+            assert_eq!(nlset.search(s), m);
+            if let Some((start, end)) = m {
+                assert!(nlset.contains(Newline::try_from(&s[start..end]).unwrap()));
+            }
         }
-    }
 
-    #[rstest]
-    #[case(NewlineSet::ASCII, "foo\r\nbar", Some((3, 5)))]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
-    #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some((3, 4)))]
-    #[case(Newline::CrLf.into(), "foo\r\nbar", Some((3, 5)))]
-    #[case(NewlineSet::ASCII, "foo\n\rbar", Some((4, 5)))]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\n\rbar", Some((3, 4)))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\n\rbar", Some((4, 5)))]
-    #[case(Newline::CarriageReturn.into(), "foo\n\rbar", Some((4, 5)))]
-    #[case(Newline::CrLf.into(), "foo\n\rbar", None)]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\nbar", Some((3, 4)))]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\nbar", None)]
-    #[case(Newline::CarriageReturn.into(), "foo\nbar", None)]
-    #[case(Newline::CrLf.into(), "foo\nbar", None)]
-    #[case(Newline::LineFeed | Newline::CrLf, "foo\rbar", None)]
-    #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some((3, 4)))]
-    #[case(Newline::CarriageReturn.into(), "foo\rbar", Some((3, 4)))]
-    #[case(Newline::CrLf.into(), "foo\rbar", None)]
-    #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some((3, 6)))]
-    fn test_newline_set_rsearch(
-        #[case] nlset: NewlineSet,
-        #[case] s: &str,
-        #[case] m: Option<(usize, usize)>,
-    ) {
-        assert_eq!(nlset.rsearch(s), m);
-        if let Some((start, end)) = m {
-            assert!(nlset.contains(Newline::try_from(&s[start..end]).unwrap()));
+        #[rstest]
+        #[case(NewlineSet::ASCII, "foo\r\nbar", Some((3, 5)))]
+        #[case(Newline::LineFeed | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\r\nbar", Some((3, 5)))]
+        #[case(Newline::CarriageReturn.into(), "foo\r\nbar", Some((3, 4)))]
+        #[case(Newline::CrLf.into(), "foo\r\nbar", Some((3, 5)))]
+        #[case(NewlineSet::ASCII, "foo\n\rbar", Some((4, 5)))]
+        #[case(Newline::LineFeed | Newline::CrLf, "foo\n\rbar", Some((3, 4)))]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\n\rbar", Some((4, 5)))]
+        #[case(Newline::CarriageReturn.into(), "foo\n\rbar", Some((4, 5)))]
+        #[case(Newline::CrLf.into(), "foo\n\rbar", None)]
+        #[case(Newline::LineFeed | Newline::CrLf, "foo\nbar", Some((3, 4)))]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\nbar", None)]
+        #[case(Newline::CarriageReturn.into(), "foo\nbar", None)]
+        #[case(Newline::CrLf.into(), "foo\nbar", None)]
+        #[case(Newline::LineFeed | Newline::CrLf, "foo\rbar", None)]
+        #[case(Newline::CarriageReturn | Newline::CrLf, "foo\rbar", Some((3, 4)))]
+        #[case(Newline::CarriageReturn.into(), "foo\rbar", Some((3, 4)))]
+        #[case(Newline::CrLf.into(), "foo\rbar", None)]
+        #[case(Newline::LineSeparator.into(), "foo\u{2028}bar", Some((3, 6)))]
+        fn rsearch(#[case] nlset: NewlineSet, #[case] s: &str, #[case] m: Option<(usize, usize)>) {
+            assert_eq!(nlset.rsearch(s), m);
+            if let Some((start, end)) = m {
+                assert!(nlset.contains(Newline::try_from(&s[start..end]).unwrap()));
+            }
         }
     }
 }
